896103ed2de7bc47ef28717fd8c675d0ace3698c
Abhishek2291 <abhishekgajera2017@gmail.com>
2023-02-28 09:38:20 +0530

    fix: product component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 import "./App.css";
 import Header from "./Header";
-// import Footer from './Footer'
-// import Product from './components/Product'
-// import ProductClass from './components/ProductClass';
-
-const Text = ({ name }) => {
-  return <div>This is {name}</div>;
-};
-
-const HOC = (WrapperComponent, color, backgroundColor) => {
-  return (props) => (
-    <div style={{ color: color, backgroundColor }}>
-      <WrapperComponent {...props} />
-    </div>
-  );
-};
+import Footer from './Footer'
+import Product from './components/Product'
+import ProductClass from './components/ProductClass';
 
 const App = () => {
   const username = "Hotel";
@@ -24,14 +12,11 @@ const App = () => {
     email: "admin@admin.com",
   };
 
-  const Comp = HOC(Text, "blue", "black");
-  const Comp2 = HOC(Text, "red", "green");
-
   return (
     <div className="content">
       <Header user={user} data={data} name={username} />
-      <Comp name="4546" />
-      <Comp2 name="789" />
+      <Product />
+      <Footer />
     </div>
   );
 };
